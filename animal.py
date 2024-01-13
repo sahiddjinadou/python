5086c2febd171db8dd13de6c3750490410549985
SAHID DJINADOU <djinadousahid@gmail.com>
2024-01-13 12:19:37 +0100

    mon premier commit

---
+++ animal.py
@@ -0,0 +1,16 @@
+class Animal : 
+    def __init__(self, nom, category) :
+        self.nom = nom
+        self.category = category
+        
+    def affiche_animale (self):
+        print(f'Caci est un {self.category}. il s\'appelle {self.nom}')
+
+class Chien(Animal):
+    def __init__(self, nom, category,poids,race):
+        super().__init__(nom, category)
+        self.poids = poids
+        self.race = race
+        
+    def affiche_animale(self):
+        super().affiche_animale() 
